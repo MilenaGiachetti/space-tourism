8016a02cd647ab3c9cf7d804550ae27d5cadb594
MilenaGiachetti <giachettimilena@gmail.com>
2022-08-21 00:43:16 -0300

    Move assets based on importation needs & add pages data

--- config-overrides.js
+++ config-overrides.js
@@ -1,14 +1,15 @@
 const path = require('path');
 
 module.exports = function override(config) {
-  config.resolve = {
-    ...config.resolve,
-    alias: {
-      ...config.alias,
-      '@assets': path.resolve(__dirname, 'src/assets'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@pages': path.resolve(__dirname, 'src/pages'),
-    },
-  };
-  return config;
+	config.resolve = {
+		...config.resolve,
+		alias: {
+			...config.alias,
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@components': path.resolve(__dirname, 'src/components'),
+			'@data': path.resolve(__dirname, 'src/data'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+		},
+	};
+	return config;
 };
